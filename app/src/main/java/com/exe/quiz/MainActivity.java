a69ada1ce06b9fa9d877da62c121d815ed584020
izabelqa <iza_szeww@wp.pl>
2023-10-08 13:29:01 +0200

    my-Quiz-App

---
+++ app/src/main/java/com/exe/quiz/MainActivity.java
@@ -0,0 +1,93 @@
+package com.exe.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button trueButton;
+    private Button falseButton;
+    private Button nextButton;
+    private TextView questionText;
+
+    private int currentIndex = 0;
+    private int correctAnswers = 0;
+    private Question[] questions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        trueButton = findViewById(R.id.trueB);
+        falseButton = findViewById(R.id.falseB);
+        nextButton = findViewById(R.id.nextB);
+        questionText = findViewById(R.id.question_text);
+
+        questions = new Question[]{
+                new Question(R.string.quest1, true),
+                new Question(R.string.quest2, false),
+                new Question(R.string.quest3, true),
+                new Question(R.string.quest4, false),
+                new Question(R.string.quest5, false)
+        };
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(true);
+            }
+        });
+
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(false);
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentIndex = (currentIndex + 1) % questions.length;
+                displayQuestion();
+            }
+        });
+        displayQuestion();
+
+    }
+    private void checkAnswer(boolean userAnswer) {
+        boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+        int resultMessageId = 0;
+        if (userAnswer == correctAnswer) {
+            correctAnswers++;
+            resultMessageId = R.string.correct_answer;
+        } else {
+            resultMessageId = R.string.incorrect_answer;
+        }
+        Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
+    }
+
+    private void displayQuestion() {
+        if (currentIndex < questions.length) {
+            questionText.setText(questions[currentIndex].getQuestionId());
+        } else {
+            String resultMessage;
+            if(correctAnswers >=3 ){
+                resultMessage = "Gratulacje! Masz " + correctAnswers + "poprawnych odpowiedzi";
+            } else {
+                resultMessage = "Spróbuj jeszcze raz! Masz tylko " + correctAnswers + "poprawne odpowiedzi";
+            }
+            questionText.setText(resultMessage);
+            trueButton.setEnabled(false);
+            falseButton.setEnabled(false);
+            nextButton.setEnabled(false);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/exe/quiz/Question.java
@@ -0,0 +1,18 @@
+package com.exe.quiz;
+
+public class Question {
+    private int questionId;
+    private boolean trueAnswer;
+
+    public Question (int questionId, boolean trueAnswer){
+        this.questionId = questionId;
+        this.trueAnswer = trueAnswer;
+    }
+    public boolean isTrueAnswer() {
+        return trueAnswer;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+}
